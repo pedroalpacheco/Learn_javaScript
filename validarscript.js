6b887ce0c624f95605fe24dd49a785b79a039414
pedroalpacheco <pedro.pacheco.a@gmail.com>
2018-08-06 10:15:21 -0300

    Validando campo2

--- validarscript.js
+++ validarscript.js
@@ -3,6 +3,7 @@ function validar(){
     var valor = document.getElementById("numero").value;
 
     if(valor.length > 2){
+        alert("ERRO : Você digitou número com mais de 2 algarismos!");
         return false;
     } else {
         return true;
