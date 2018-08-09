405312e389a954a3c9d24d425d58e49c6aafca7d
pedroalpacheco <pedro.pacheco.a@gmail.com>
2018-08-09 17:00:06 -0300

    Nulls - undefined

---
+++ null_undefile.js
@@ -0,0 +1,13 @@
+let valor //Não inicializada
+console.log(valor)
+
+valor = null //Ausencia de valor
+console.log(valor)
+
+//console.log(valor.toString())//erro!
+
+const produto = {}
+
+produto.preco = 3.50
+
+console.log(produto.preco)
---
+++ objeto.js
@@ -0,0 +1,19 @@
+
+//Criar objeto de forma dinamica
+const prod1 = {}
+prod1.nome = 'Ultra celular'
+prod1.preco = 5623.23
+prod1['Desconto legal'] = 0.40 //Evitar atributos com espaço
+
+
+console.log(prod1)
+
+//Outra forma de criar objeto
+
+const prod2 = {
+    nome: 'Camiseta sueter',
+    preco: 79.56
+
+}
+
+console.log(prod2)
