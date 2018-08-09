9e7f86ad63aecf1ee13f99f1a05e98e77a3dd4be
pedroalpacheco <pedro.pacheco.a@gmail.com>
2018-08-09 14:57:06 -0300

    Booleanos

---
+++ booleanos.js
@@ -0,0 +1,18 @@
+let isativo = false
+
+isativo = true
+
+console.log(isativo)
+
+isativo = 0
+
+console.log(!!isativo)
+
+
+//Verifica string vazia
+console.log('String não vazia = '+!!'')
+
+//Verifica se string esta vazia - operação lógica numa string
+let nome = ''
+
+console.log(nome||'Desconhecido')
