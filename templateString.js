d7a51b2db566fd48f9549812a88e7369cdc574ff
pedroalpacheco <pedro.pacheco.a@gmail.com>
2018-08-09 14:34:39 -0300

    Strings e templates

---
+++ templateString.js
@@ -0,0 +1,14 @@
+const nome = 'Helio'
+const concatenacao = 'ola ' + nome + '!'
+
+const template = `
+    Ola
+    ${nome}
+`
+console.log(concatenacao, template)
+
+console.log(`1 + 1 = ${1+1}`)
+
+const up = texto => texto.toUpperCase()
+
+console.log(`Ei... ${up('cuidado')}`)
