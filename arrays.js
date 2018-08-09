a4f87a980238462777cc5d9a72b64b5f7ba7237b
pedroalpacheco <pedro.pacheco.a@gmail.com>
2018-08-09 15:41:19 -0300

    Arrays

---
+++ arrays.js
@@ -0,0 +1,20 @@
+const valores = [7.4,2.5,10,36]
+//console.log(valores[0],valores[3])
+
+valores[4] = 10
+
+valores.concat('2')
+
+//Também adiciona valor
+console.log(valores.concat(2))
+
+console.log(valores.length)
+
+//Adiciona valor
+valores.push(2.563)
+
+console.log(valores)
+
+delete valores[0]
+
+console.log(valores)
