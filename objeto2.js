30e584ecfda93bc80525bf2f1df5fe8b9b2a6736
pedroalpacheco <pedro.pacheco.a@gmail.com>
2018-08-13 10:48:36 -0300

    Novidades

---
+++ objeto2.js
@@ -0,0 +1,4 @@
+console.log(typeof object)
+console.log(typeof new object)
+
+const Cliente = function(){}
---
+++ parNomeValor.js
@@ -0,0 +1,25 @@
+//par nome/valor
+
+const saudacao = 'ola'
+
+function exec(){
+    const saudacao = 'Bom dia'
+    return saudacao
+}
+
+const cliente = {
+    nome: 'Pedro',
+    idade: '40',
+    peso:'70,',
+    endereco :{ 
+        logradouro:'Rua tal ruas',
+        numero:479,
+
+
+
+    }
+}
+
+console.log(saudacao)
+console.log(exec())
+console.log(cliente)
