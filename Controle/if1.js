7e010c06c320b3baa105c3d1caa4138b36507013
pedroalpacheco <pedro.pacheco.a@gmail.com>
2018-08-15 10:25:51 -0300

    Analise de notas com if-else if

---
+++ Controle/if1.js
@@ -0,0 +1,21 @@
+/*function analisaNotas(nota){
+    if(nota>=7){
+        console.log("Acima da média!")
+    }else{
+        console.log("Abaixo da média!")
+    }
+}
+
+analisaNotas(7.6)
+analisaNotas(5.3)*/
+
+function vervalidade(valor){
+    if(valor){
+        console.log("Valor verdadeiro é :" + valor)
+    }else{
+        console.log("valor não digitado!")
+    }
+}
+
+vervalidade()
+vervalidade(52)
