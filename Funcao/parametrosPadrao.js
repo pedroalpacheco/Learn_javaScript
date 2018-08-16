450749137755ac28e439d6da51d956d8a7510fa4
pedroalpacheco <pedro.pacheco.a@gmail.com>
2018-08-16 10:54:36 -0300

    This&Bind

---
+++ Funcao/parametrosPadrao.js
@@ -0,0 +1,25 @@
+//Gerar valor padrão
+function soma1(a,b,c){
+    a = a || 1
+    b = b || 1
+    c = c || 1
+    //return a+b+c
+    console.log(a+b+c)
+}
+
+function soma2(a,b,c){
+    a = a !== undefined ? a : 1
+    b = 1 in arguments ? b : 1
+    c = isNaN(c) ? 1 : c
+
+    return a + b + cancelAnimationFrame
+}
+//console.log(soma2(), soma2(3), soma2(1,2,3))
+
+
+//valor padrão ES2015
+function soma3(a=1,b=1,c=1){
+    return a + b + c
+}
+
+console.log(soma3(), soma3(3))
