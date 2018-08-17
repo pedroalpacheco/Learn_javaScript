6f54ac316d19cb7a6e427c0041d70c9c46c50d32
pedroalpacheco <pedro.pacheco.a@gmail.com>
2018-08-17 16:55:55 -0300

    Objetos

---
+++ Basico/Objetos.js
@@ -0,0 +1,18 @@
+
+//Exemplo d orientação a objetos
+
+var jedi = {
+  name: "Yoda",
+  age: 899,
+  color: 'Blue',
+  //talk: function () { alert("another... Sky... walk..."); }
+  talk: function () { console.log("another... Sky... walk..."); }
+};
+
+console.log(jedi.name)
+
+console.log(jedi.age)
+
+console.log(jedi.talk())
+
+console.log(jedi.color)
---
+++ Objeto2.js
@@ -0,0 +1,14 @@
+//Herança de objetos
+
+var person = {
+    age: 122
+};
+
+person.name = {
+    first: "Jeanne",
+    last: "Calment"
+};
+
+console.log(person.age)
+
+console.log(person.name.first)
